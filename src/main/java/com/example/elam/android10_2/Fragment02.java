bb38dded8c1a40491c9f5d7935a1b3402a18fe14
Navinbala5 <bnavin28@gmail.com>
2017-07-17 20:03:10 +0530

    Add files via upload

---
+++ src/main/java/com/example/elam/android10_2/Fragment02.java
@@ -0,0 +1,29 @@
+package com.example.elam.android10_2;
+
+/**
+ * Created by elam on 10-07-2017.
+ */
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+
+
+public class Fragment02 extends Fragment {
+    View rootView;
+    MainActivity activity = (MainActivity) getActivity();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        rootView = inflater.inflate(R.layout.fragment_fragment02, container, false);
+
+        return rootView;
+    }
+}
